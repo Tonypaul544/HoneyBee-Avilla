abb993a75380860ec4e22dbd090d3446ee11df1c
Tonypaul544 <paulnwodotochukwu180@gmail.com>
2026-02-07 18:40:27 +0100

    images

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -14,14 +14,14 @@ export default function Gallery() {
       <h1 className="flex max-sm:m-auto max-sm:text-5xl max-sm:text-[25px] mb-5 text-5xl font-bold text-white text-center w-full justify-center pt-10 pb-5 m-auto">Gallery</h1>
       <p className="flex text-lg text-center font-semibold m-auto w-content p-1">Capturing the energy, passion, and moments that define the stage</p>
 
-      <div className="flex flex-wrap justify-center gap-5 m-auto items-center justify-center mt-10 w-[90%] h-auto">
-        <img src={honb} alt="Gallery Image" className="w-[20%] h-[300px]" />
-        <img src={honb2} alt="Gallery Image" className="w-[20%] h-[300px]" />
-        <img src={honb3} alt="Gallery Image" className="w-[20%] h-[300px]" />
-        <img src={honb4} alt="Gallery Image" className="w-[20%] h-[300px]" />
-        <img src={honb5} alt="Gallery Image" className="w-[21%] h-[300px]" />
-        <img src={honb6} alt="Gallery Image" className="w-[20%] h-[300px]" />
-        <img src={honb7} alt="Gallery Image" className="w-[20%] h-[300px]" />
+      <div className="flex max-sm:flex-col max-sm:items-center max-sm:gap-5 flex-wrap justify-center gap-5 m-auto items-center justify-center mt-10 w-[90%] h-auto">
+        <img src={honb} alt="Gallery Image" className="w-[20%] h-[450px] min-w-[250px] max-sm:w-full" />
+        <img src={honb2} alt="Gallery Image" className="w-w-[20%] h-[450px] min-w-[250px] max-sm:w-full " />
+        <img src={honb3} alt="Gallery Image" className="w-w-[20%] h-[450px] min-w-[250px] max-sm:w-full" />
+        <img src={honb4} alt="Gallery Image" className="w-w-[20%] h-[450px] min-w-[250px] max-sm:w-full" />
+        <img src={honb5} alt="Gallery Image" className="w-[20%] h-[450px] min-w-[250px] max-sm:w-full" />
+        <img src={honb6} alt="Gallery Image" className="w-w-[20%] h-[450px] min-w-[250px] max-sm:w-full" />
+        <img src={honb7} alt="Gallery Image" className="w-w-[20%] h-[450px] min-w-[250px] max-sm:w-full" />
       </div>
     </div>
   )
